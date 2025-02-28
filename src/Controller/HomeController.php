315dbee0feb72146bb681f02fe3581651bebcaf0
Olga Fedotova <olgaalex1994@gmail.com>
2025-02-28 15:39:44 +0100

    redirect after connexion

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,9 +2,10 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class HomeController extends AbstractController
 {
@@ -12,6 +13,10 @@ final class HomeController extends AbstractController
     #[Route('/home', name: 'app_home')]
     public function index(): Response
     {
+        if ($this->getUser()) {
+            // Redirige directement vers la page des films
+            return $this->redirectToRoute('app_film_index');
+        }
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
