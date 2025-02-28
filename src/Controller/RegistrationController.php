010c5ce59071e3e93a41ef0e70199ed007999619
Olga Fedotova <olgaalex1994@gmail.com>
2025-02-28 16:06:47 +0100

    creation compte

---
+++ src/Controller/RegistrationController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegistrationType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+final class RegistrationController extends AbstractController
+{
+    #[Route('/', name: 'app_register')]
+    public function register(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $user = new User();
+        $form = $this->createForm(RegistrationType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Définir le rôle par défaut en ABONNÉ
+            $user->setRoles(['ROLE_USER']);
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Votre compte a été créé avec succès. Vous pouvez maintenant vous connecter.');
+
+            return $this->redirectToRoute('app_login');
+        }
+
+        return $this->render('registration/register.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
