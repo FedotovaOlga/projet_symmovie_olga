701d13649e5b96de988557c7db12f8d054eaaca9
Olga Fedotova <olgaalex1994@gmail.com>
2025-02-28 12:24:13 +0100

    Entities User, Film and Favori created

---
+++ src/Entity/Favori.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\FavoriRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: FavoriRepository::class)]
+class Favori
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $dateAjout = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDateAjout(): ?\DateTimeInterface
+    {
+        return $this->dateAjout;
+    }
+
+    public function setDateAjout(?\DateTimeInterface $dateAjout): static
+    {
+        $this->dateAjout = $dateAjout;
+
+        return $this;
+    }
+}
